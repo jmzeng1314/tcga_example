a21d000787e7d2326239395ca2fef5333f25551a
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2018-08-07 23:45:36 +0800

    简化代码

--- step0-install-packages.R
+++ step0-install-packages.R
@@ -14,43 +14,16 @@
 # https://github.com/jmzeng1314/biotrainee 
 
 options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
-if(! require("devtools")) install.packages("devtools")
-if(! require("reshape2")) install.packages("reshape2")
-if(! require("ggplot2")) install.packages("ggplot2")
-if(! require("pheatmap")) install.packages("pheatmap")
-if(! require("ggfortify")) install.packages("ggfortify")
-if(! require("stringr")) install.packages("stringr")
-if(! require("survival")) install.packages("survival")
-if(! require("survminer")) install.packages("survminer")
-if(! require("lars")) install.packages("lars")
-if(! require("glmnet")) install.packages("glmnet")
-
-if(! require("timeROC")) install.packages("timeROC")
-if(! require("ggpubr")) install.packages("ggpubr")
-
-if(! require("randomForest")) install.packages("randomForest")
-if(! require("ROCR")) install.packages("ROCR")
-if(! require("genefilter")) install.packages("genefilter")
-if(! require("Hmisc")) install.packages("Hmisc")
- 
-if(! require("caret")) install.packages("caret")
-if(! require("genefilter")) install.packages("genefilter")
-if(! require("Hmisc")) install.packages("Hmisc")
-
-
-library(devtools) 
-source("http://bioconductor.org/biocLite.R") 
 options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/") 
-library(BiocInstaller)
-biocLite(c('airway','DESeq2','edgeR','limma'))
+source("http://bioconductor.org/biocLite.R") 
 
-if(! require("CLL")) biocLite("CLL")
-if(! require("org.Hs.eg.db")) biocLite(org.Hs.eg.db)
-if(! require("maftools")) biocLite("maftools")
+packs = c("devtools", "reshape2", "ggplot2", "pheatmap", "ggfortify", "stringr", "survival",
+          "survminer", "lars", "glmnet", "timeROC", "ggpubr", "randomForest", "ROCR", "genefilter",
+          "Hmisc", "caret", "airway","DESeq2","edgeR","limma", "CLL", "org.Hs.eg.db", "maftools")
 
+if(! require(pacman)) install.packages("pacman", dependencies = TRUE)
+pacman::p_load(packs, dependencies=TRUE, character.only = TRUE)
 
-
-
-
-
-
+# check
+pacman::p_loaded(packs, character.only = TRUE)
+all(pacman::p_loaded(packs, character.only = TRUE))
